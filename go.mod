46a917c9a6888c0eda8322fac40c0046a968cd96
Diogo Cezar Teixeira Batista <diogo.batista@meifacil.com>
2021-08-24 15:06:01 -0300

    chore: starting mongo configurations

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/pilu/config v0.0.0-20131214182432-3eb99e6c0b9a // indirect
 	github.com/pilu/fresh v0.0.0-20190826141211-0fa698148017 // indirect
 	github.com/satori/go.uuid v1.2.0
+	go.mongodb.org/mongo-driver v1.7.1
 )
---
+++ mongo/mongo.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func db() *mongo.Client {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+	return client
+}
